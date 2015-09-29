c8774fb3df9a27c72b359767788fedf76215b4c4
tinyhill <tinyhill@163.com>
2015-09-30 01:30:38 +0800

    Add PostComment model

--- models/Post.js
+++ models/Post.js
@@ -18,11 +18,15 @@ Post.add({
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: { type: Types.CloudinaryImage },
 	content: { type: Types.Html, wysiwyg: true, height: 400 },
-	categories: { type: Types.Relationship, ref: 'PostCategory', many: true }
-});
-
-Post.schema.virtual('content.full').get(function() {
-	return this.content.extended || this.content.brief;
+	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
+	type: { type: Types.Select, options: 'article, product, registration', default: 'article', index: true },
+	images: { type: Types.CloudinaryImages, dependsOn: { type: ['product', 'registration'] } },
+	time: { type: String, dependsOn: { type: ['product', 'registration'] } },
+	location: { type: String, dependsOn: { type: ['product', 'registration'] } },
+	contact: { type: String, dependsOn: { type: ['product', 'registration'] } },
+	cost: { type: Number, dependsOn: { type: ['product'] } },
+	limit: { type: Number, dependsOn: { type: ['registration'] } }
+	// products: { type: Types.Relationship, ref: 'Product', many: true, dependsOn: { type: ['product'] } }
 });
 
 Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
